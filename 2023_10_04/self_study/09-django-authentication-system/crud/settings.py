523d68e0454a13c24d936cc02db9de9e677636de
Lim-seunghwan <hwan7185@gmail.com>
2023-10-04 16:13:54 +0900

    강사님 따라감..

--- 2023_10_04/self_study/09-django-authentication-system/crud/settings.py
+++ 2023_10_04/self_study/09-django-authentication-system/crud/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-AUTH_USER_MODEL = 'accounts.User'
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
